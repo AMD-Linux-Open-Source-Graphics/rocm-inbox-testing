e0e1ceabe486764d2e2541c1d08da20c36594cf2
Tim Flink <Tim.Flink@amd.com>
2026-05-27 14:56:10 -0500

    fixing scripting errors and typos in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -15,22 +15,24 @@ if [ -z $ROCMTESTTAG ];
 then
     echo "by default, the entire selftest suite will be run. This can take over 12 hours"
     echo "to run a subset of the suite, set the ENV variable \$ROCMTESTTAG at runtime"
-    echo "waiting 10 seconds before starting execution"
-    wait 10
+    echo "waiting 5 seconds before starting execution"
+    echo ""
+    sleep 5
     export ROCMTESTTAG="selftest"
 fi
 
 echo "running tests with tag '$ROCMTESTTAG'"
+echo ""
 
-if [ -z /usr/bin/tmt ];
+if [ ! -f /usr/bin/tmt ];
 then
     echo "tmt is not installed. make sure it is installed at /usr/bin/tmt before running this script"
     exit 1
 fi
 
-if [ -z /usr/bin/testrig ];
+if [ ! -f /usr/bin/testrig ];
 then
-    echo "testrig is not installed. make sure it is installed at /usr/bin/tmt before running this script"
+    echo "testrig is not installed. make sure it is installed at /usr/bin/testrig before running this script"
     exit 1
 fi
 
